802d7ea1c8a9fb078bf8e360e61fc8308ba814d0
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-18 20:56:54 +0300

    anasayfa oluşturuldu

---
+++ src/sayfalar/Anasayfa.jsx
@@ -0,0 +1,15 @@
+function Anasayfa() {
+  return (
+    <section>
+      <h1>Ana sayfa</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore alias,
+        quae nam eos illum ea rerum nulla distinctio itaque facere
+        necessitatibus, nobis tempore! Numquam aspernatur laboriosam magni
+        voluptas, officia repudiandae.
+      </p>
+    </section>
+  );
+}
+
+export default Anasayfa;
