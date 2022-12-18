4cfb45d5b87bfd8a49307400a274ef5e898ba025
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-18 18:01:27 +0300

    memo örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import React from "react";
+import C1 from "./C1";
 
 function App() {
-  return <div className='App'>merhaba</div>;
+  const [yazi, yaziGuncelle] = React.useState("");
+
+  return (
+    <div className='App'>
+      Merhaba!
+      <input
+        type='text'
+        value={yazi}
+        onChange={(olay) => {
+          yaziGuncelle(olay.target.value);
+        }}
+      />
+      <C1 />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/C1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function C1() {
+  return <p>Ben C1</p>;
+}
+
+export default React.memo(C1);
