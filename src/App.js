c6ec4c1a06e6ca9db7cfb9ce212709e16bb5ee02
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-18 20:57:10 +0300

    app oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sablon from "./sayfalar/Sablon";
+import Anasayfa from "./sayfalar/Anasayfa";
+import Hakkimizda from "./sayfalar/Hakkimizda";
+import Iletisim from "./sayfalar/Iletisim";
+import Sayfayok from "./sayfalar/Sayfayok";
 
 function App() {
-  return <div className='App'>merhaba</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Sablon />}>
+          <Route index element={<Anasayfa />} />
+          <Route path='blogs' element={<Hakkimizda />} />
+          <Route path='contact' element={<Iletisim />} />
+          <Route path='*' element={<Sayfayok />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
