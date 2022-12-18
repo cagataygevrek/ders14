620eeec23ee245de59d511dcb4890ca511662891
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-18 20:57:21 +0300

    hakkimizda oluşturuldu

---
+++ src/sayfalar/Hakkimizda.jsx
@@ -0,0 +1,21 @@
+function Hakkimizda() {
+  return (
+    <section>
+      <h1>Hakkımızda</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias
+        repellendus modi accusantium beatae amet! Nisi expedita enim laboriosam,
+        ratione deserunt suscipit dolore officia in exercitationem minus magnam
+        sunt explicabo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nemo
+        quaerat accusamus laborum possimus, quam odio maiores at excepturi
+        sapiente facere nulla ipsa debitis eligendi error rem laboriosam. Eaque,
+        ad.
+      </p>
+    </section>
+  );
+}
+
+export default Hakkimizda;
