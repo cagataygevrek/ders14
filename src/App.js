c0348e89e2a31142719735ee86b2bfe2beb9aeb1
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-18 17:11:38 +0300

    useref dom eleman örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import React from "react";
 
 function App() {
-  return <div className='App'>merhaba</div>;
+  let eleman1 = React.useRef(); //eleman1.current =
+
+  function yaziGuncelle() {
+    eleman1.current.textContent = "İkinci yazı";
+  }
+
+  return (
+    <div className='App'>
+      <p ref={eleman1}>İlk yazı</p>
+
+      <button onClick={yaziGuncelle}>Güncelle</button>
+    </div>
+  );
 }
 
 export default App;
